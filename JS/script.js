1fafdcb056b8c7b7f42b6002bb7c86576c8c7cde
Tanay Dalal <tdalal@hawk.iit.edu>
2023-10-14 16:35:24 -0500

    save button and store

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,8 @@
 function changeText() {
     document.getElementById('demoText').textContent = "Text has been changed!";
 }
+document.getElementById('saveButton').addEventListener('click', function() {
+    let inputValue = document.querySelector('input[type="text"]').value;
+    localStorage.setItem('savedInput', inputValue);
+    alert('Data saved to localStorage');
+});
