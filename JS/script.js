e134b62ef1ea709d98ff7e357c97466a72d50974
Tanay Dalal <tdalal@hawk.iit.edu>
2023-10-14 16:35:55 -0500

    animate

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,17 @@ document.getElementById('saveButton').addEventListener('click', function() {
     localStorage.setItem('savedInput', inputValue);
     alert('Data saved to localStorage');
 });
+function animateDiv() {
+    let div = document.getElementById('animatedDiv');
+    let pos = 0;
+    let id = setInterval(frame, 5);
+
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            div.style.left = pos + 'px';
+        }
+    }
+}
